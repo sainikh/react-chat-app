bc549f7cbb99b0b840f3a5385730634fa12fb99f
Sai Nikhil A <47454954+sainikh@users.noreply.github.com>
2022-05-29 19:45:44 +0530

    created login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,23 @@
 import { render } from '@testing-library/react';
 import './login.css';
 
+
 function Login()
 {
       
 
    return(
-      <div className=' bg-gray-700 h-screen w-screen text-white'>
-sai
+      <div className='  bg-gray-700 h-screen w-fill text-white'>
+         <div>
+            <br/>
+            <div className=' flex justify-center p-10'>
+            <button className=' bg-slate-600 p-4 align-middle rounded-md'>Login in with Google
+            <img src={process.env.PUBLIC_URL+"/google-logo-png.png"} width="192" alt="second logo" ></img>
+            </button>
+            </div>
+            <div className=' p-5 text-center text-xl '><p>Please create your google account </p></div>
+            <div className=' p-1 text-center text-xl '><p>if not present </p></div>
+            </div>
    </div>);
 }
 
--- src/components/nav.js
+++ src/components/nav.js
@@ -9,7 +9,7 @@ function Login()
       <div className='flex flex-row bg-blue-500 h-fill w-fill text-white p-4 '>
        
           <div className='flex flex-auto w-56 p-4 text-2xl font-bold'>SHACHAT </div>
-          <div className='flex flex-row w-20'><button className=' bg-slate-500 rounded-3xl p-3 border-4'>Login</button></div>
+          <div className='flex flex-row w-20'><button className=' bg-slate-500 rounded-3xl p-3 border-4'>Logout</button></div>
           <div className=' m-1'></div>
 
    </div>
