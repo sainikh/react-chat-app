348432c238a2c48e7c46df5f7ecf08a5539b4d1e
Sai Nikhil A <47454954+sainikh@users.noreply.github.com>
2022-05-29 22:15:33 +0530

    changed name to shardings and changed logout button as sm border

--- src/components/nav.js
+++ src/components/nav.js
@@ -3,13 +3,13 @@ import './login.css';
 
 function Login()
 {
-      
+      const user= null;
 
    return(
       <div className='flex flex-row bg-blue-500 h-fill w-fill text-white p-4 '>
        
-          <div className='flex flex-auto w-56 p-4 text-2xl font-bold'>SHACHAT </div>
-          <div className='flex flex-row w-20'><button className=' bg-slate-500 rounded-3xl p-3 border-4'>Logout</button></div>
+          <div className='flex flex-auto w-56 p-4 text-2xl font-bold'>Shardings </div>
+          {user? <div></div>:<div className='flex flex-row w-20'><button className=' h-15 bg-slate-500 rounded-md p-4 border-2'>Logout</button></div>}
           <div className=' m-1'></div>
 
    </div>
