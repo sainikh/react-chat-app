cb5d14418530a738964d6d92064d178ab7c034b1
Sai Nikhil A <47454954+sainikh@users.noreply.github.com>
2022-05-30 16:24:36 +0530

    just edited Body UI and chat message

--- src/components/body.js
+++ src/components/body.js
@@ -13,8 +13,8 @@ function body() {
       }
 
     return (
-         <div className = " flex  flex-row h-screen " >
-        <div className="flex flex-col w-16 justify-between items-center p-4">
+         <div className = "  flex  flex-row h-screen " >
+        <div className=" hidden flex flex-col w-16 justify-between items-center p-4">
             <div className="flex flex-col space-y-2">
             <a><div className=" rounded-full bg-gray-400 w-8 h-8"></div></a>
             <a><div className=" rounded-full bg-gray-400 w-8 h-8"></div></a>
@@ -23,7 +23,7 @@ function body() {
           
            
              </div>
-        <div className =" flex flex-col w-64 bg-amber-300 p-1">
+        <div className ="hidden flex flex-col w-64 bg-amber-300 p-1">
             <div>
             <a><div className =" flex flex-col  bg-slate-800 p-3">asdasdasdasda sdad  asd  asd  ssadddas asdsd asdas </div></a>
             </div>
@@ -39,8 +39,30 @@ function body() {
      
         <div className="flex-auto bg-green-300 relative">
           <div className= "flex flex-col h-auto p-4 m-2 bg-slate-200"> Tech Point </div> 
-          <div className= "flex flex-col h-[32rem] p-4 m-2  bg-slate-200"> Tech Point </div>
-          <div className="absolute bottom-0 left-0 w-full"><Sildebar /></div>
+          <div className= "flex flex-col h-[32rem] p-4 m-2  bg-slate-200 overflow-auto"> 
+          <div className='flex flex-col space-y-4'>
+            <div className='grid justify-items-start bg-slate-200'>
+            <img src={process.env.PUBLIC_URL+"/google-logo-png.png"} width="100"  alt="second logo" />
+            <div className= " rounded-2xl p-3  bg-slate-400 max-w-md ">hi this is sai nikhil A  asdsd sd adasdsa dad sd asd asd sad as das das d  d as dsa dassdsadas das dasd asd sad asd sd asd sadas d asd asd as </div>
+            </div>
+         
+
+            <div className='grid justify-items-end bg-slate-200'>
+            <img src={process.env.PUBLIC_URL+"/google-logo-png.png"} width="100"  alt="second logo" />
+            <div className= " rounded-2xl p-3  bg-slate-400 max-w-md ">Im fine how are you doing.asd adsd sdas ds ad asdas das dsad asdsd asd asdas dsa dsa das das das dasdasdsad asdasdasdasdasdasd asdsa das dasdasdasdasd sads dd </div>
+            
+            </div>
+
+
+            
+
+         
+
+          
+          </div>
+          
+          </div>
+          <div className=" w-full"><Sildebar /></div>
           
           </div>
          
